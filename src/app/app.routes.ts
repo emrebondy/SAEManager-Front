fd028c6fceca4802a0c0cfa65c7cbbae0dd2c041
emrebondy <gucluemre976@gmail.com>
2025-02-11 22:33:28 +0100

    formulaire creerSAE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { CreerSAEComponent } from './creer-sae/creer-sae.component';
 
 export const routes: Routes = [
     {path: '', component: InscriptionComponent},
-    {path: 'Accueil', component: AccueilComponent}
+    {path: 'Accueil', component: AccueilComponent},
+    {path:'CreerSAE', component: CreerSAEComponent}
 ];
---
+++ src/app/creer-sae/creer-sae.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creer-sae',
+  imports: [],
+  templateUrl: './creer-sae.component.html',
+  styleUrl: './creer-sae.component.css'
+})
+export class CreerSAEComponent {
+
+}
