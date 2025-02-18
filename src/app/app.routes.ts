c84b538a67165f00e9614cca438c3f5a036310cd
emrebondy <gucluemre976@gmail.com>
2025-02-19 00:03:17 +0100

    merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { CreerSAEComponent } from './creer-sae/creer-sae.component';
+import { SaeComponent } from './sae/sae.component';
 
 export const routes: Routes = [
     {path: '', component: InscriptionComponent},
     {path: 'Accueil', component: AccueilComponent},
-    {path:'CreerSAE', component: CreerSAEComponent}
+    {path:'CreerSAE', component: CreerSAEComponent},
+    {path:'SAE', component: SaeComponent}
 ];
